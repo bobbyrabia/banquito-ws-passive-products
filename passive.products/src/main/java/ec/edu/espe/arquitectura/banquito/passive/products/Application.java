15f5f042d72599ba4b7f2caee253170cda330f33
bobbyrabia <rpfreire1@espe.edu.ec>
2023-07-11 12:53:35 -0500

    initial configuration start init

---
+++ passive.products/src/main/java/ec/edu/espe/arquitectura/banquito/passive/products/Application.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.banquito.passive.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
